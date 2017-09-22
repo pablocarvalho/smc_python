d150e67b523d2d1dc4ceb4fa4ca2a05ea5aa205d
pablocarvalho <pablomccarvalho@gmail.com>
2017-09-22 00:15:18 -0300

    implementação do condicional e algumas melhorias nas funções que executam blocos de comandos e blocos de expressões booleanas

--- smc.py
+++ smc.py
@@ -86,10 +86,11 @@ class SMCmachine:
 				self.print_state()
 			
 			elif(current_node.type == 'booleanexpressionnot'):
+				self.c.push(str(current_node.leaf))
 				child0 = self.load_program(current_node.children[0]) 					
 				if(child0 is not None):			
 					self.c.push(child0)
-				self.c.push(str(current_node.leaf))
+				
 				
 				self.print_state()
 
@@ -105,9 +106,26 @@ class SMCmachine:
 				if(child0 is not None):			
 					self.c.push(child0)
 				self.c.push(current_node.leaf[0])	
-				
-				
 				self.print_state()
+
+
+			elif(current_node.type == 'conditional'):
+
+				child2 = self.load_program(current_node.children[2])
+				if(child2 is not None):			
+					self.c.push(child2)
+				self.c.push(current_node.leaf[2])
+
+				child1 = self.load_program(current_node.children[1])
+				if(child1 is not None):			
+					self.c.push(child1)
+
+				self.c.push(current_node.leaf[1])	
+				child0 = self.load_program(current_node.children[0]) 					
+				if(child0 is not None):			
+					self.c.push(child0)
+				self.c.push(current_node.leaf[0])	
+				self.print_state()	
 
 			elif(current_node.type == 'command_block' or current_node.type == 'booleanexpression_block'):
 				blockContent = Stack()				
@@ -119,10 +137,8 @@ class SMCmachine:
 				self.print_state()
 
 				block_elements_list = self.c.items[ (first_par_pos+1) : (last_par_pos) ]   
-				#print "block: " + str(block_elements_list)
 				blockContent.items = block_elements_list
 				
-				#print "inicio:" + str(first_par_pos) + " fim:" + str(last_par_pos)
 
 				del self.c.items[first_par_pos:last_par_pos+1]
 
@@ -133,8 +149,7 @@ class SMCmachine:
 					commandBlock = BooleanBlock(blockContent)
 					self.c.push(commandBlock)
 
-				#for i in range(first_par_pos, last_par_pos-2):					
-				#	del self.c.items[i]					
+							
 
 				self.print_state()
 
@@ -163,21 +178,32 @@ class SMCmachine:
 			if(control == 'add' or control == 'div' or control == 'mul' or control == 'sub'):
 				self.evaluate_binary_op(control)	
 				
-			elif(control == 'and' or control == 'or'):
+			elif(control == 'and' or control == 'or' or control == 'eq'):
 				self.evaluate_binary_bool(control)	
 				
 			elif(control=='tt' or control=='ff'):
 				value = self.s.push(control);					
 				self.print_state();
 
+			elif(control =='not'):
+				value = self.s.pop();
+				if(value == 'tt'):
+					self.s.push('ff')
+				else:
+					self.s.push('tt')
+
+
 			elif(control=='while'):
 				self.print_state();
 				booleanblock = self.c.pop()
 
-				#booleanblock_copy = BooleanBlock(list(booleanblock.content.items))				
-				#self.eval_control(booleanblock_copy)
-				self.eval_while_booleanblock(booleanblock)
+				
+				boolstack = Stack()
+				boolstack.items = list(booleanblock.content.items)
+				booleanblock_copy = BooleanBlock(boolstack)
+				self.eval_boolean_block(booleanblock_copy)
 
+				self.print_state();
 				eval_result = self.s.pop()
 				do_word = self.c.pop()
 				self.print_state();
@@ -188,7 +214,6 @@ class SMCmachine:
 				cmdstack.items = list(commandblock.command.items)
 				commandblock_copy = Command(cmdstack)	
 
-				#print commandblock_copy.command.items			
 
 				if(eval_result == 'tt'):
 					self.eval_command_block(commandblock_copy)
@@ -199,8 +224,32 @@ class SMCmachine:
 					self.c.push(control)
 					self.print_state()
 
+			elif(control == 'if'):
+				booleanblock = self.c.pop()
+				self.print_state()
+				self.eval_boolean_block(booleanblock)
 
-			
+				self.print_state()
+				eval_result = self.s.pop()
+				self.print_state()
+				word_then = self.c.pop()
+				self.print_state()
+
+				commandblock = self.c.pop()				
+				self.print_state()
+				word_else = self.c.pop()
+				self.print_state()
+				elsecommandblock = self.c.pop()
+				self.print_state()
+
+				if(eval_result == 'tt'):
+					self.eval_command_block(commandblock)
+				else:
+					self.eval_command_block(elsecommandblock)
+					
+				self.print_state()
+
+					
 			elif(control == ':='):
 				variable = self.c.pop();
 				value = self.s.pop();
@@ -221,32 +270,13 @@ class SMCmachine:
 			else:
 				self.s.push(control)
 				self.print_state();
-		
-
-	def eval_while_booleanblock(self, booleanblock):
-		booleanList = booleanblock.content.items
-		self.c.items.extend(booleanList)
-		self.print_state();
-
-		localControl = ""
-		
-		while(localControl != "do"):
-			localControl = self.c.pop();
-			
-			if(localControl=='tt' or localControl=='ff'):
-				value = self.s.push(localControl);					
-				self.print_state();
-				
-			elif(localControl == 'and' or localControl == 'or'):
-				self.evaluate_binary_bool(localControl)
-
-		self.c.push("do")
-
+	
 	def eval_command_block(self, commandblock):
 		self.debug = False
+
 		actualControlStack = list(self.c.items)
 		self.c.items = commandblock.command.items
-		print "command_block" + str(actualControlStack)
+		#print "command_block" + str(actualControlStack)
 
 		while(not self.c.isEmpty() ):
 
@@ -257,8 +287,21 @@ class SMCmachine:
 
 		self.debug = True
 
-		
+	def eval_boolean_block(self, booleanblock):
+		self.debug = False
 
+		actualControlStack = list(self.c.items)
+		self.c.items = booleanblock.content.items
+		#print "boolean_block" + str(actualControlStack)
+
+		while(not self.c.isEmpty() ):
+
+			control = self.c.pop()
+			self.eval_control(control)
+
+		self.c.items = actualControlStack
+
+		self.debug = True
 
 	def var_exists(self,varName):
 		index = 0
@@ -292,28 +335,41 @@ class SMCmachine:
 
 	def evaluate_binary_bool(self,operator):
 		op1 = self.s.pop()		
-		op2 = self.s.pop()		
-		if(isinstance(op1,str) and not (op1 == 'tt' or op1 == 'ff')):
-			self.loadFromMemoryToS(op1)
-			op1 = self.s.pop();		
-		if(isinstance(op2,str)and not (op2 == 'tt' or op2 == 'ff')):
-			self.loadFromMemoryToS(op2)
-			op2 = self.s.pop();
-		temp1 = op1 == 'tt'
-		temp2 = op2 == 'tt'
-		self.print_state()
-		if(operator == 'and'):
-			if(temp1 and temp2):
-				self.s.push('tt')
-			else:
-				self.s.push('ff')
-		if(operator == 'or'):
-			if(temp1 or temp2):
-				self.s.push('tt')
-			else:
-				self.s.push('ff')
-		
+		op2 = self.s.pop()
 
+		if(operator == "eq"):		
+			if(isinstance(op1,str)):
+				self.loadFromMemoryToS(op1)
+				op1 = self.s.pop();		
+			if(isinstance(op2,str)):
+				self.loadFromMemoryToS(op2)
+				op2 = self.s.pop();
+
+			if(op1 == op2):
+				self.s.push('tt')
+			else:
+				self.s.push('ff')
+
+		else:
+			if(isinstance(op1,str) and not (op1 == 'tt' or op1 == 'ff')):
+				self.loadFromMemoryToS(op1)
+				op1 = self.s.pop();		
+			if(isinstance(op2,str)and not (op2 == 'tt' or op2 == 'ff')):
+				self.loadFromMemoryToS(op2)
+				op2 = self.s.pop();
+			temp1 = op1 == 'tt'
+			temp2 = op2 == 'tt'
+			self.print_state()
+			if(operator == 'and'):
+				if(temp1 and temp2):
+					self.s.push('tt')
+				else:
+					self.s.push('ff')
+			if(operator == 'or'):
+				if(temp1 or temp2):
+					self.s.push('tt')
+				else:
+					self.s.push('ff')
 		
 		self.print_state();
 
@@ -322,11 +378,6 @@ class SMCmachine:
 		memCell = self.m[address]
 		self.s.push(memCell.data)
 		self.print_state()
-
-
-
-
-
 
 class Stack:
      def __init__(self):
