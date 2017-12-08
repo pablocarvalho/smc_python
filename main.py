f54ec7edb1c1b8b7b43cd222e01bd158ac5e83dd
pablocarvalho <pablomccarvalho@gmail.com>
2017-12-08 07:18:07 -0200

    modificações na main

--- main.py
+++ main.py
@@ -5,17 +5,20 @@ import smc
 import esmc
 
 def run():
-    # run_smc()
-    run_lparser()
+    run_smc()
+    #run_lparser()
 
 def run_smc():
 	calc = smcparser.Calc()
 	tree = calc.run()
 	nd.print_tree(tree)
 	machine = smc.SMCmachine()
-	a = machine.load_program(tree)
+
+	print "\n\nExecutando SMC ---------"
+	machine.load_and_run(tree)
+	#a = machine.load_program(tree)
 	machine.print_state()
-	machine.run_program()
+	#machine.run_program()
 
 def run_lparser():
 	calc = lparser.Calc()
